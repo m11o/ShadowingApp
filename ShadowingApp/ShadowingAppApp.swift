622981e3c2f447d9d14dff93a2929f4f1aeac411
masahito1997 <velonica.gale2227@gmail.com>
2021-03-25 14:35:14 +0900

    Initial Commit

---
+++ ShadowingApp/ShadowingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShadowingAppApp.swift
+//  ShadowingApp
+//
+//  Created by 大迫雅仁 on 2021/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ShadowingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
